ac15a93d3b147969c74ba40d30488658ba9da1a7
Devora Nagel <devoranagel@gmail.com>
2020-12-08 16:42:48 -0600

    added frame

--- src/main/java/pong/PongFrame.java
+++ src/main/java/pong/PongFrame.java
@@ -1,4 +1,18 @@
 package pong;
 
-public class PongFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class PongFrame extends JFrame {
+
+    public PongFrame(PongView pongView, PaddleKeyListener paddleListener) throws HeadlessException {
+        super();
+        setSize(800,400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Pong");
+        setLayout(new BorderLayout());
+
+        add(pongView, BorderLayout.CENTER);
+        addKeyListener(paddleListener);
+    }
 }
