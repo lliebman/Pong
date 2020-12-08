79f89b033fb7b105ec073aa9cdacad67a36a248e
Devora Nagel <devoranagel@gmail.com>
2020-12-08 17:17:09 -0600

    added view

--- src/main/java/pong/PongView.java
+++ src/main/java/pong/PongView.java
@@ -1,4 +1,34 @@
 package pong;
 
-public class PongView {
+import javax.swing.*;
+import java.awt.*;
+
+public class PongView extends JComponent {
+    private final Environment environment;
+
+    public PongView(Environment environment) {this.environment = environment;}
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        paintBackground(g);
+        paintPaddles(g);
+        paintBall(g);
+
+        //scoreboard?
+        //demarcation lines?
+    }
+
+    private void paintBackground(Graphics g) {
+        g.setColor(Color.black);
+        g.fillRect(0,0,getWidth(),getHeight());
+    }
+
+    private void paintPaddles(Graphics g) {
+        g.setColor(Color.white);
+    }
+
+    private void paintBall(Graphics g) {
+        g.setColor(Color.white);
+    }
 }
