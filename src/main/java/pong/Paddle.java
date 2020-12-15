dc5b0f042e7ca3a899efe15103a2da62281ba054
rachel613g <rachelgutmann613@gmail.com>
2020-12-15 01:29:33 -0500

    added padding to paddle position

--- src/main/java/pong/Paddle.java
+++ src/main/java/pong/Paddle.java
@@ -2,15 +2,16 @@ package pong;
 
 public class Paddle {
     private int x, y;
+    private final int PADDING = 10;
 
     public Paddle(Player player){
         y = Environment.HEIGHT/2;
         if (player.equals(Player.USER)){
             //set human paddle to right side of board
-            x = Environment.WIDTH - PongView.PADDLE_WIDTH;
+            x = Environment.WIDTH - PongView.PADDLE_WIDTH - PADDING;
         } else {
             //set computer paddle to left side of board
-            x = PongView.PADDLE_WIDTH;
+            x = PongView.PADDLE_WIDTH + PADDING;
         }
     }
 
